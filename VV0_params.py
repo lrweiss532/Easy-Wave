7c9312016a738fd3018542aed1098e1e2de2452b
AlexBourassa <abourassa@uchicago.edu>
2019-07-22 13:41:42 -0500

    Added some VV0 parametters

---
+++ VV0_params.py
@@ -0,0 +1,181 @@
+
+PL1 = {
+	"D": 1.336e9, #Hz
+	"d_para": 2.65e-2, #Hz/(V/m)
+	"epsilon_para": 0, #Unknown
+	"d_perp": 0, #Unknown
+	"epsilon_perp": 3e9, #Hz/strain (2-4 Ghz/strain so average of 3)
+	"E0": 0,
+	"gamma_e" : 2.8e6,#Hz/Gauss
+
+	"SiIIb": {
+		"Axx": 9e6, #Hz
+		"Ayy": 9e6, #Hz
+		"Azz": 9e6, #Hz
+		"tetha": 0, #Unknown
+		"gamma_n": -8.465e2 #Hz/Gauss
+		},
+
+	"CI": {
+		"Axx": 53e6, #Hz
+		"Ayy": 50e6, #Hz
+		"Azz": 110e6, #Hz
+		"tetha": 73, #Degrees
+		"gamma_n": 10.705e2 #Hz/Gauss
+		},
+
+	"SiI": {
+		"Axx": 3e6, #Hz
+		"Ayy": 3e6, #Hz
+		"Azz": 3e6, #Hz
+		"tetha": 0, #Unknown
+		"gamma_n": -8.465e2 #Hz/Gauss
+		},
+
+	"SiIIa": {
+		"Axx": 12e6, #Hz
+		"Ayy": 12e6, #Hz
+		"Azz": 12e6, #Hz
+		"tetha": 0, #Unknown
+		"gamma_n": -8.465e2 #Hz/Gauss
+		}
+	
+}
+
+
+PL2 = {
+	"D": 1.305e9, #Hz
+	"d_para": 1.61e-2, #Hz/(V/m)
+	"epsilon_para": 0, #Unknown
+	"d_perp": 0, #Unknown
+	"epsilon_perp": 3e9, #Hz/strain (2-4 Ghz/strain so average of 3)
+	"E0": 0,
+	"gamma_e" : 2.8e6,#Hz/Gauss
+
+	"SiIIb": {
+		"Axx": 10e6, #Hz
+		"Ayy": 10e6, #Hz
+		"Azz": 10e6, #Hz
+		"tetha": 0, #Unknown
+		"gamma_n": -8.465e2 #Hz/Gauss
+		},
+
+	"CI": {
+		"Axx": 47e6, #Hz
+		"Ayy": 45e6, #Hz
+		"Azz": 104e6, #Hz
+		"tetha": 73, #Degrees
+		"gamma_n": 10.705e2 #Hz/Gauss
+		},
+
+	"SiI": {
+		"Axx": 3e6, #Hz
+		"Ayy": 3e6, #Hz
+		"Azz": 3e6, #Hz
+		"tetha": 0, #Unknown
+		"gamma_n": -8.465e2 #Hz/Gauss
+		},
+
+	"SiIIa": {
+		"Axx": 13e6, #Hz
+		"Ayy": 13e6, #Hz
+		"Azz": 13e6, #Hz
+		"tetha": 0, #Unknown
+		"gamma_n": -8.465e2 #Hz/Gauss
+		},
+}
+
+
+PL3 = {
+	"D": 1.222e9, #Hz
+	"d_para": 0.41e-2, #Theoretical number (Experimental number is <3)
+	"epsilon_para": 0, #Unknown
+	"d_perp": 32.3e-2, #Hz/(V/m)
+	"epsilon_perp": 3e9, #Hz/strain (2-4 Ghz/strain so average of 3)
+	"E0": 82e6, #Hz
+	"gamma_e" : 2.8e6,#Hz/Gauss
+
+	"CIa": {
+		"Axx": 51e6, #Hz (Theoretical)
+		"Ayy": 52e6, #Hz (Theoretical)
+		"Azz": 118e6, #Hz (Theoretical)
+		"tetha": 2, #Degrees (Theoretical)
+		"gamma_n": 10.705e2 #Hz/Gauss
+		},
+
+	"CIb": {
+		"Axx": 50e6, #Hz
+		"Ayy": 50e6, #Hz
+		"Azz": 109e6, #Hz
+		"tetha": 70, #Degrees (Theoretical)
+		"gamma_n": 10.705e2 #Hz/Gauss
+		}
+	
+}
+
+
+PL4 = {
+	"D": 1.334e9, #Hz
+	"d_para": 0.44e-2, #Hz/(V/m)
+	"epsilon_para": 0, #Unknown
+	"d_perp": 28.5e-2, #Hz/(V/m)
+	"epsilon_perp": 3e9, #Hz/strain (2-4 Ghz/strain so average of 3)
+	"E0": 18.7e6, #Hz
+	"gamma_e" : 2.8e6,#Hz/Gauss
+	
+	"CIa": {
+		"Axx": 52e6, #Hz
+		"Ayy": 52e6, #Hz
+		"Azz": 110e6, #Hz
+		"tetha": 0, #Degrees (Theoretical)
+		"gamma_n": 10.705e2 #Hz/Gauss
+		},
+
+	"CIb": {
+		"Axx": 48e6, #Hz
+		"Ayy": 45e6, #Hz
+		"Azz": 109e6, #Hz
+		"tetha": 70, #Degrees (Theoretical)
+		"gamma_n": 10.705e2 #Hz/Gauss
+		}
+
+}
+
+
+PL5 = {
+	"D": 1.373e9, #Hz
+	"d_para": 1.5e-2, #Experimental number is <3
+	"epsilon_para": 0, #Unknown
+	"d_perp": 32.5e-2, #Hz/(V/m)
+	"epsilon_perp": 0, #Unknown
+	"E0": 16.5e6, #Hz
+	"gamma_e" : 2.8e6,#Hz/Gauss
+	
+}
+
+
+PL6 = {
+	"D": 1.365e9, #Hz
+	"d_para": 0.96e-2, #Hz/(V/m)
+	"epsilon_para": 0, #Unknown
+	"d_perp": 0, #Unknown
+	"epsilon_perp": 0, #Unknown
+	"E0": 0, #Unknown
+	"gamma_e" : 2.8e6,#Hz/Gauss
+
+	"SiIIa": {
+		"Axx": 0, #Unknown
+		"Ayy": 0, #Unknown
+		"Azz": 12.5e6, #Hz
+		"tetha": 0, #Unknown
+		"gamma_n": -8.465e2 #Hz/Gauss
+		},
+
+	"SiIIb": {
+		"Axx": 0, #Unknown
+		"Ayy": 0, #Unknown
+		"Azz": 9.6e6, #Hz
+		"tetha": 0, #Unknown
+		"gamma_n": -8.465e2 #Hz/Gauss
+		}	
+}
